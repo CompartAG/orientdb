d496ab2c40645d5ced50ff53038a228179947863
dta <dta@compart.com>
2016-09-21 16:20:09 +0200

    ObjectField handling strategy selected via a config value

--- object/src/main/java/com/orientechnologies/orient/object/enhancement/OObjectFieldHandler.java
+++ object/src/main/java/com/orientechnologies/orient/object/enhancement/OObjectFieldHandler.java
@@ -5,16 +5,33 @@
 
 package com.orientechnologies.orient.object.enhancement;
 
+import com.orientechnologies.orient.core.config.OGlobalConfiguration;
+
 /**
- * Wraps a {@link OObjectFieldHandlingStrategy} to be accessed statically.
+ * Wraps a {@link OObjectFieldHandlingStrategy} to be accessed statically.<br/>
+ * The strategy to be used is set via {@link OGlobalConfiguration#OBJECT_BINARY_MAPPING} and the possible values are
+ * {@link #SIMPLE},{@link #SINGLE_ORECORD_BYTES} or {@link #SPLIT_ORECORD_BYTES}.
  * 
  * @author dta
  */
 public class OObjectFieldHandler {
 
+    public static final int SIMPLE = 0;
+    public static final int SINGLE_ORECORD_BYTES = 1;
+    public static final int SPLIT_ORECORD_BYTES = 2;
+
     private static final OObjectFieldHandlingStrategy STRATEGY;
     static {
-        STRATEGY = new SmartOObjectFieldHandlingStrategy();
+        switch (OGlobalConfiguration.OBJECT_BINARY_MAPPING.getValueAsInteger()) {
+        case SINGLE_ORECORD_BYTES:
+            STRATEGY = new SmartOObjectFieldHandlingStrategy();
+            break;
+        case 2:
+        case 0:
+        default:
+            STRATEGY = new SimpleOObjectFieldHandlingStrategy();
+            break;
+        }
     }
 
     public static final OObjectFieldHandlingStrategy getStrategy() {
